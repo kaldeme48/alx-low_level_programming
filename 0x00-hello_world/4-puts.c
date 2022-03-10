92a9da2c198712132f3995533609a1677df3e639
kaldeme48 <kaldeme4828@yahoo.com>
2022-03-10 13:15:45 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#!/bin/bash
-#include  <stdio.h>
+#include <stdio.h>
 
 /**
-* main - entry point
+* main Entry point
 *
-* Return : Always 0 (Sucess)
+* Return : Always 0 (Success)
 */
-int  main(void)
+int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+      puts("\"Programming is like building a multilingual puzzle");
+      return (0);
 }
