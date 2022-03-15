3c0f892cac8ee269c12c7cb2c67fb7d9fd50fbd9
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-15 21:04:46 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
-* main - Prints putchar
+* main - Entry point
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char *sh = "_putchar";
+
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
 _putchar('\n');
+
 return (0);
 }
-[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[A[A[C[C[C[C[C[C[C[C[C[C[alx
-
