985b47e644f6edde95333c9c2cbab1734802c162
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-16 01:39:29 +0300

    add task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - adds 2 integers and returns result
+* @one: first number input
+* @two: second number input
+* Return: one + two
+*/
+int add(int one, int two)
+{
+return (one + two);
+}
