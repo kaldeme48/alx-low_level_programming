eabebf55a240a20bfe55308d7cf7f7368788a4e9
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-15 19:41:28 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+_putchar('_');
+[A[B_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
