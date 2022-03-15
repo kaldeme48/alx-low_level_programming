3213a142aeaf1c6b36ce0e6f62285ba3e20711e9
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-15 20:34:25 +0300

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* main - prints alphabet
+* print_alphabet - checks alphabet
 *
 * Return: Always 0 (Success)
 */
+
 void print_alphabet(void)
 {
+
 int n;
 
-for(n = 'a'; n <= 'z'; n++)
+for (n = 'a'; n <= 'z'; n++)
 {
 _putchar(n);
 }
