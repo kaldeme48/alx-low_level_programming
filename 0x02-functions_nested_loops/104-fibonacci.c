8b6894d7e02f6bd38fa2c1a2b0b086da1063ed8e
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-16 19:31:59 +0300

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,8 @@ int main(void)
 	unsigned long int bef = 1;
 	unsigned long int aft = 2;
 	unsigned long int l = 1000000000;
-	unsigned lo	unsigned long int bef2;
+	unsigned long int bef1;
+	unsigned long int bef2;
 	unsigned long int aft1;
 	unsigned long int aft2;
 
