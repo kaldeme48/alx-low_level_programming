96fdd0219865d7ab1685462c8523cb71c0156271
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-16 11:50:12 +0300

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* main - entry point
+* purpose: first 98 fibonacci
+* Return: 0
 */
 int main(void)
 {
+int i = 0;
+unsigned long int a = 0, b = 1, next = 0;
 
+while(i < 98)
+{
+next = a + b;
+a = b;
+b = next;
+printf("%lu", next);
+
+if (i < 97)
+printf(", ");
+i++;
+}
+putchar('\n');
 return (0);
 }
