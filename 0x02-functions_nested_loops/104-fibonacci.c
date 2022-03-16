44988b708c2d1b23f14260afd513432ce1e6ba4e
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-16 19:08:40 +0300

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
+
 /**
-* main - entry point
-* purpose: first 98 fibonacci
-* Return: 0
+* main - fibonacci
+*
+* Return: (success)
 */
 int main(void)
 {
-int i = 0;
-unsigned long int a = 0, b = 1, next = 0;
+unsigned long int i;
+unsigned long int bef = 1;
+unsigned long int aft = 2;
+unsigned long int l = 1000000000;
+unsigned long int bef1;
+unsigned long int bef2;
+unsigned long int aft1;
+unsigned long int aft2;
 
-while(i < 98)
+printf("%lu", bef);
+
+for (i = 1; i < 91; i++)
 {
-next = a + b;
-a = b;
-b = next;
-printf("%lu", next);
-
-if (i < 97)
-printf(", ");
-i++;
+printf(", %lu", aft);
+aft += bef;
+bef = aft - bef;
 }
-putchar('\n');
+
+bef1 = (bef / 1);
+bef2 = (bef % 1);
+aft1 = (aft / 1);
+aft2 = (aft % 1);
+
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", aft1 + (aft2 / 1));
+printf("%lu", aft2 % 1);
+aft1 = aft1 + bef1;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
+}
+printf("\n");
 return (0);
 }
