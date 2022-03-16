b73719676f5b4ca0738b5632d6b6bc5a939773ea
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-16 11:44:43 +0300

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
 * main - entry point
-* purpose - sum of all multiple of 3 and 5 below 1024
+* purpose: print sum of multiple of 3 and 5 below 1024
 * Return: 0
 */
 int main(void)
@@ -12,11 +12,10 @@ int sum = 0;
 
 while (c < 1024)
 {
-if (c % 3 == 0 || c % 5 ==0)
+if (c % 3 == 0 || c % 5 == 0)
 {
 sum += c;
 }
-
 c++;
 }
 printf("%i\n", sum);
