f6a6a6e0d380e947d67a8cf334d67dbfa37edbba
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-21 11:37:14 +0300

    string reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: str input
+ * Return: string in reverse
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	for (n = (count - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
