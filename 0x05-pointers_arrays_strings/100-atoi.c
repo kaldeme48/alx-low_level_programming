b8d40b155dddff1072ea64cab5d587b5bdb64ad3
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-03-21 23:08:09 +0300

    adv update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, res = 0, i;
+	int sign = 1, res = 0, i, j;
 
 	for (i = 0; !(s[i] >= 48 && s[i] <= 57); i++)
 	{
@@ -17,7 +17,7 @@ int _atoi(char *s)
 		}
 	}
 
-	for (int j = i; s[j] >= 48 && s[j] <= 57; j++)
+	for (j = i; s[j] >= 48 && s[j] <= 57; j++)
 	{
 		res *= 10;
 		res += (s[j] - 48);
