f1984a5bfd7fc3e166380f6bb7b0345dbdfb5ed0
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-04-04 22:35:42 +0300

    concat commit2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,52 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
+
 /**
- * str_concat - Entry point
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
  */
-char *str_concat(char *s1, char *s2)
+
+
+int _strlen(char *s)
 {
-	int end1, end2, i = 0;
-	char *array;
-
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
-
-	for (end1 = 0; end1 <= *s1; end1++)
-	{
-	}
-
-	for (end2 = 0; end2 <= *s2; end2++)
-	{
-	}
-
-	array = malloc(sizeof(char) * (end1 + end2 + 1));
-
-	if (array == NULL)
-		return (NULL);
-
-	while (*s1)
-	{
-		array[i] = *s1;
-		i++;
-		s1++;
-	}
-
-	while (*s2)
-	{
-		array[i] = *s2;
-		i++;
-		s2++;
-	}
-	return (array);
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
 }
 
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+int end1, end2, i;
+char *m;
+
+if (s1 == NULL)
+	s1 = "\0";
+if (s2 == NULL)
+	s2 = "\0";
+
+end1 = _strlen(s1);
+end2 = _strlen(s2);
+m = malloc((end1 + end2) *sizeof(char) + 1);
+if (m == 0)
+	return (0);
+
+for (i = 0; i <= end1 + end2; i++)
+{
+	if (i < end1)
+		m[i] = s1[i];
+	else
+		m[i] = s2[i - end1];
+}
+m[i] = '\0';
+return (m);
+}
