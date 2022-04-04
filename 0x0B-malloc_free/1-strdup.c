d2b78ad2fcf92a3df7ef95615e3c6cbd4c714430
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-04-04 22:11:34 +0300

    create array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - Entry point
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int i, end;
+	char *array;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (end = 0; end <= *str; end++)
+	{
+	}
+
+	end += 1;
+	array = malloc(sizeof(char) * end);
+
+	for (i = 0; i < end; i++)
+		array[i] = str[i];
+
+	if (array == NULL)
+		return (NULL);
+	return (array);
+}
+
+
