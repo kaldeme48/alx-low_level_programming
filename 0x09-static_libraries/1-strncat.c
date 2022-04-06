ddde9a4a7e72f4a8e9e84cc40bfa8756e43dccd2
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-04-06 11:13:12 +0300

    strncat

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: where we copy to
+ * @src: where we copy from
+ * @n: input of bytes
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, c;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (c = 0; c < n; c++)
+	{
+		dest[i + c] = src[c];
+		if (src[c] == '\0')
+			c = n;
+	}
+
+	return (dest);
+}
