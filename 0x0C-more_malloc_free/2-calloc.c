a6f2b1d2dcc1fe326fdec1dd10b932f6a3def277
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-04-07 12:22:48 +0300

    calloc edit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,7 +2,7 @@
 
 /**
  * *_memset - fills memory with a constant byte.
- * @s: pointer to put the constant
+ * @s: pointer to putthe constant
  * @b: constant
  * @n: max bytes to use
  * Return: s
