7f8b507a3be623a497d332ed886d757471c3dcf9
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-04-14 22:53:33 +0300

    print numbers commit edit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,37 +1,33 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * print_strings - Entry Point
- * @separator: comma space
- * @n: number of elements
- * Return: void
+ * print_strings - prints all strings with a separator
+ * @separator: separe strings
+ * @n: int, number of undefined arguments
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
-	char *str;
+va_list list;
+unsigned int i;
+char *c;
+va_start(list, n);
 
-	if (separator == NULL)
-		return;
+for (i = 0; i < n; i++)
+{
+	c = va_arg(list, char*);
+	if (c != NULL)
+		printf("%s", c);
+	else
+		printf("(nil)");
 
-	va_start(valist, n);
+	if (i != n - 1 && separator != NULL)
+		printf("%s", separator);
+}
+va_end(list);
 
-	for (i = 0; i < n; i++)
-	{
-		str = va_arg(valist, char *);
-		if (str == NULL)
-		{
-			str = "(nil)";
-		}
-		printf("%s", str);
-		if (i != (n - 1))
-		{
-			printf("%s", separator);
-		}
-	}
-	printf("\n");
-
-	va_end(valist);
+putchar('\n');
 }
