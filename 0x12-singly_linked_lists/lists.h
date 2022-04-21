8695147f1543530c2fe850e7745a2a9f7cf20998
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-04-22 01:12:23 +0300

    print list final commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 /**
  * struct list_s - singly linked list
@@ -23,6 +23,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-int _strlen(const char *kd);
 
 #endif
