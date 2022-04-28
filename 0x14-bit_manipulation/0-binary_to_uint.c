760e3ef0cb67db49aed3154f2d26e1a27dcd3427
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-04-29 00:30:52 +0300

    binry to uint edit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * binary_to_uint - Entry point
- * @b: string containing the binary number
+ * @b: pointer to a string of 0 and 1 chars
  * Return: the converted number
- */
+*/
+
 unsigned int binary_to_uint(const char *b)
 {
-        int i;
-        unsigned int dec_val = 0;
+	unsigned int kd = 0;
 
-        if (!b)
-                return (0);
-
-        for (i = 0; b[i]; i++)
-        {
-                if (b[i] < '0' || b[i] > '1')
-                        return (0);
-                dec_val = 2 * dec_val + (b[i] - '0');
-        }
-        return (dec_val);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		kd = kd << 1;
+		if (*b == '1')
+			kd = (kd | 1);
+		b++;
+	}
+	return (kd);
 }
-
