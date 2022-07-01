68490a20ad8a228cc892476ec5d7325e222fe3a4
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-07-01 22:23:41 +0300

    hash commit locally tested

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,30 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - function that creates a hash table
- * @size: size of the array
- * Return: pointer to newly created hash table
+ * hash_table_create - creates a hash table
+ * @size: size of the array of the hash table
+ *
+ * Return: Pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int idx = 0;
-	hash_table_t *new_table = NULL;
+	hash_table_t *ht;
 
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
+	if (size == 0)
 		return (NULL);
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->array == NULL)
-	{
-		free(new_table);
-		new_table = NULL;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
 		return (NULL);
-	}
-	while (idx < size)
-	{
-		new_table->array[idx] = NULL;
-		idx++;
-	}
-	new_table->size = size;
-	return (new_table);
+
+	ht->size = size;
+	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+		return (NULL);
+
+	return (ht);
 }
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,31 +1,53 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - function that prints a hash table
- * @ht: the hash table
- * Return: a dictionary format of the hash table
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the hash_node_t list to print
+ */
+void print_list(hash_node_t *h)
+{
+	while (h)
+	{
+		printf("'%s': '%s'", h->key, h->value);
+		if (h->next)
+			printf(", ");
+		h = h->next;
+	}
+}
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i = 0;
-	hash_node_t *temp = NULL;
+	unsigned long int i;
+	hash_node_t *node = NULL;
+	char *last_key = NULL;
+	unsigned long int index;
 
-	if (ht == NULL)
+	if (!ht)
 		return;
 
-	printf("{");
-	while (i < ht->size)
+	for (i = 0; i < ht->size; i++)
 	{
-		temp = ht->array[i];
-		while (temp)
-		{
-			printf("'%s' : '%s'", temp->key, temp->value);
-			if (ht->array[i + 1] == NULL)
-				break;
-			printf(", ");
-			temp = temp->next;
-		}
-		i++;
+		if (ht->array[i] != NULL)
+			node = ht->array[i];
 	}
+
+	printf("{");
+
+	if (node)
+	{
+		last_key = node->key;
+		index = key_index((const unsigned char *)last_key, ht->size);
+		for (i = 0; i < ht->size; i++)
+		{
+			print_list(ht->array[i]);
+			if (ht->array[i] && i < index)
+				printf(", ");
+		}
+	}
+
 	printf("}\n");
 }
